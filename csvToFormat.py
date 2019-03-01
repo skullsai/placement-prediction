478baf01129f77e2ee677c244ab51b398853958b
Sai Vish <saivish@ymail.com>
2019-03-01 13:44:50 +0530

    Add files via upload

---
+++ csvToFormat.py
@@ -0,0 +1,11 @@
+def csvToFormat(work_df):
+    
+    for i in range(len(work_df)):
+        if (i%1000==0):
+            print(i)
+        work_df['companies'][i]=removeWeirdChars(work_df['companies'][i])
+        work_df.skills[i]=work_df.skills[i].lstrip('[').rstrip(']').replace("'","").split(',')
+        work_df.schools[i]=work_df.schools[i].lstrip('[').rstrip(']').replace("'","").split(',')  
+        work_df.interests[i]=work_df.interests[i].lstrip('[').rstrip(']').replace("'","").split(',')   
+        work_df.majors[i]=work_df.majors[i].lstrip('[').rstrip(']').replace("'","").split(',') 
+    return work_df
